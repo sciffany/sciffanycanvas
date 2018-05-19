7edfc302313978374cd63ae84c85dd2022b79ddd
Tiffany Chong <sciffany@gmail.com>
2018-05-19 19:46:16 +0800

    debugging

--- config/environments.rb
+++ config/environments.rb
@@ -2,7 +2,7 @@
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
+	"connection"
 	ActiveRecord::Base.establish_connection(
 			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
 			:host     => db.host,
