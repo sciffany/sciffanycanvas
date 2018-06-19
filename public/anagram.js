fd3ce82f5ed42a37d3528582e3c0860727beed69
Tiffany Chong <sciffany@gmail.com>
2018-06-19 17:23:48 +0800

    updating log

--- public/anagram.js
+++ public/anagram.js
@@ -211,7 +211,7 @@ function tryGuess(index, character){
 
                 });
 **/
-            $.ajax({url: "anagram.js", success: function(result){
+            $.ajax({url: "public/anagram.js", success: function(result){
                     alert("he");
                     $("#div1").html(result);
                 }});
